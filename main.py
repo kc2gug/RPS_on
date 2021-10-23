afd8822fb9432a37a73b1355e6167ecc62ebed59
kc2gug <kc2gug@gmail.com>
2021-10-23 09:49:33 -0400

    The demo for class

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+rpsp_dict  = {"Rock": ("Scissors", "Lizard"),
+              "Paper": ("Rock", "Spock"),
+              "Scissors": ("Paper", "Lizard")}
+
+game_note = """============================
+= GAME: ROCK PAPER SCISSORS + 2
+= 
+= CHOOSE ROCK/PAPER/SCISSORS to see if you win!!
+= """
+print(game_note)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def play_the_game(rpsp2):
+    comp_val, val = random.choice(list(rpsp2.items()))
+
+    print("=\n= LETS PLAY!!")
+
+    for names, values in rpsp2.items():
+        print("=\t\t"+names)
+
+    user_val = input("= Enter one of the choices > ")
+
+    print("=\n= You chose:      "+str(user_val))
+    print("= Computer chose: "+comp_val)
+    print("=")
+
+    f = open("dataset.dat", "a")
+
+    to_write=""
+    try:
+        if comp_val == user_val:
+            to_write="=    TIE GAME"
+        elif comp_val in rpsp2[user_val]:
+            to_write="=    YOU WIN!!!!"
+        else:
+            to_write="=    ALL OF YOUR BASE ARE BELONG TO US!! :-) !!"
+    except:
+        to_write="= \""+str(user_val)+"\" IS NOT AN OPTION.\n= Please play again!"
+
+    print(to_write)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+do_runrun=True
+while do_runrun:
+    play_the_game(rpsp_dict)
+    play_again = input("=\n= Play Again?[Y/N] > ")
+    if play_again != "Y":
+        do_runrun=False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
